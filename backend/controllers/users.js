7341ff37080c9a61bf6d4a050023d37ab3ce5268
opigon1 <step.rindin@yandex.ru>
2023-11-23 21:00:21 +0300

    test

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -145,6 +145,7 @@ module.exports.login = (req, res, next) => {
             httpOnly: true,
             sameSite: true,
             maxAge: 3600000 * 24 * 7,
+            secure: true,
           })
           .status(200)
           .send({
@@ -156,6 +157,7 @@ module.exports.login = (req, res, next) => {
       if (err.name === "ValidationError") {
         return next(
           new BAD_REQUEST("Поле email или password не должны быть пустыми")
+        );
       } else {
         return next(new UNAUTHORIZED("Передан неккоректный email"));
       }
