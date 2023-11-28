e6e29b00febc4f45214f0876da8b2d687f53a9b4
opigon1 <step.rindin@yandex.ru>
2023-11-28 12:57:28 +0300

    pr_15_reviu

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://api.project.mesto.nomoredomainsmonster.ru";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://api.project.mesto.nomoredomainsmonster.ru";
+// const BASE_URL = "http://localhost:3000";
 function handleCheckResponse(res) {
   if (res.ok) {
     return res.json();
--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,6 +1,6 @@
 export const apiConfig = {
-  // url: "https://api.project.mesto.nomoredomainsmonster.ru/",
-  url: "http://localhost:3000/",
+  url: "https://api.project.mesto.nomoredomainsmonster.ru/",
+  // url: "http://localhost:3000/",
   headers: {
     authorization: "ab3d5963-89f3-4f42-abd5-a3c9a26d09e5",
     "content-type": "application/json",
